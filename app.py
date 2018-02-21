24c89a85ba10a198253a3f19eb929d704a76152f
landonbrand <mail@landonbrand.com>
2018-02-20 19:31:52 -0800

    test-- weird error

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask, current_app, request
 
 app = Flask(__name__)
-
+config = {
+    'DEBUG': False
+    'TESTING': False
+    'CSRF_ENABLED': True
+    'SECRET_KEY': 'this-really-needs-to-be-changed'
+    'SQLALCHEMY_DATABASE_URI': os.environ['DATABASE_URL']
+}
+app.config.from_object(config)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
---
+++ models.py
@@ -0,0 +1,15 @@
+from app import db
+from sqlalchemy.dialects.postgresql import JSON
+
+
+class Result(db.Model):
+    __tablename__ = 'results'
+
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String)
+
+    def __init__(self, text):
+        self.text = text
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
